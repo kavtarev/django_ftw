57c12d0ad45808bf6b3fb0742898b14ef1e2cb12
Ivan <ivan.kavtarev@mail.ru>
2021-08-18 21:54:41 +0300

    3

--- news/views.py
+++ news/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News_model
+
+
 # Create your views here.
 
-def index(request):
-    print(request)
-    return HttpResponse('hi pes')
+def index(request): # called action
+    news = News_model.objects.all()
+  
+    return render(request,'news/index.html',{'news':news,'title':'news list'})
 
 def test(request):
     return HttpResponse('<h1>Test page</h1>')
