ee4b4dfcd7c0508a25b833e7822a6187085276b4
Ivan <ivan.kavtarev@mail.ru>
2021-08-20 21:56:30 +0300

    adm

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import News_model
 
+class NewsAdmin(admin.ModelAdmin):
+    list_display=('header','text', 'created_at', 'changed_at', 'is_published')
+    list_display_links = ('header', 'text')
+    search_fields = ('header', 'text')
+
+  
+admin.site.register(News_model, NewsAdmin) # order is important
 # Register your models here.
--- news/models.py
+++ news/models.py
@@ -4,14 +4,16 @@ from django.db import models
 
 class News_model(models.Model): # News
     
-    header = models.CharField(max_length=151)
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    changed_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
-    is_published = models.BooleanField(default=True)
+    header = models.CharField(max_length=151,verbose_name='заголовок')
+    text = models.TextField(verbose_name='текст')
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='создан')
+    changed_at = models.DateTimeField(auto_now=True, verbose_name='изменен')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d',blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='опубликован')
 
     def __str__(self):
         return self.header
-
+    class Meta:
+        verbose_name = 'tsatsaev'
+        verbose_name_plural='kaki'
 
